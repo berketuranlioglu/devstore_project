e21c24d0e61babcf834d1e1b34f486aa2e756302
Berke Turanlıoğlu <berke.turanlioglu@gmail.com>
2021-11-27 18:01:46 +0300

    color.dart update
    Yeni bir iki renk geldi.

--- lib/utils/color.dart
+++ lib/utils/color.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryColor = Color(0xFF9441E4);
+  static const Color primaryColor = Color(0xFF9441E4); //
   static const Color secondaryColor = Color(0xFFFFFFFF);
   static const Color thirdColor = Color(0xFFEEE5F8);
   static const Color buttonBackgroundColor = Color(0xFF9441E4); //Added - Add To Cart - Log in - Sign up buttons
@@ -11,4 +11,6 @@ class AppColors {
   static const Color appBarColor = const Color(0xFF222730);
   static const Color textColor = Colors.black;
   static const Color ratingBlockColor = const Color(0xFF0CCD08); //Seller Rate
+  static const Color deliveredTextColor = const Color(0xFF34A853); //Orders Page Delivered Text Color
+  static const Color processingTextColor = const Color(0xFFE3CD00); //Orders Page Processing Text Color
 }
