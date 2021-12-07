7cf3f99bd7ea206b33dd66a542fc33d5706bc67e
dcmetiner <92755437+dcmetiner@users.noreply.github.com>
2021-12-07 21:27:47 +0300

    started working on feed view

---
+++ lib/routes/feed.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:devstore_project/utils/color.dart';
+import 'package:devstore_project/utils/styles.dart';
+import 'package:devstore_project/utils/dimension.dart';
+
+class FeedView extends StatefulWidget {
+  const FeedView({Key? key}) : super(key: key);
+
+  @override
+  _FeedViewState createState() => _FeedViewState();
+}
+
+class _FeedViewState extends State<FeedView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
