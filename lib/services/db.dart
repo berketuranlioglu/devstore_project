ef7657aa43286777761596675c9e6527d087874e
Berzan Koçdağ <berzankocdag@gmail.com>
2021-12-25 20:07:20 +0300

    Updates

--- lib/services/db.dart
+++ lib/services/db.dart
@@ -1,11 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DBService {
-  final CollectionReference userCollection =
-      FirebaseFirestore.instance.collection('users');
+  final firestoreInstance = FirebaseFirestore.instance;
 
   Future addUserAutoID(String nameSurname, String mail, String token) async {
-    userCollection
+    firestoreInstance
+        .collection("users")
         .add({'nameSurname': nameSurname, 'userToken': token, 'email': mail})
         .then((value) => print('User added'))
         .catchError((error) => print('Error: ${error.toString()}'));
@@ -13,13 +13,33 @@ class DBService {
 
   Future addUser(String nameSurname, String mail, String token, String phone,
       String username, String password) async {
-    userCollection.doc(token).set({
-      'nameSurname': nameSurname,
-      'username': username,
-      'userToken': token,
-      'phone': phone,
-      'email': mail,
-      'password': password,
-    });
+    firestoreInstance
+        .collection("users")
+        .doc(token)
+        .set({
+          'nameSurname': nameSurname,
+          'username': username,
+          'userToken': token,
+          'phone': phone,
+          'email': mail,
+          'password': password,
+          'imageUrl': 'https://i.stack.imgur.com/l60Hf.png',
+          'isActive': true,
+        })
+        .then((value) => print('User added'))
+        .catchError((error) => print('Error: ${error.toString()}'));
+  }
+
+  Future editDetails(String nameSurname, String token, String imageUrl,
+      String phone, String password) async {
+    firestoreInstance
+        .collection("users")
+        .doc(token)
+        .update({
+          'password': password,
+          'imageUrl': imageUrl,
+        })
+        .then((value) => print('User added'))
+        .catchError((error) => print('Error: ${error.toString()}'));
   }
 }
